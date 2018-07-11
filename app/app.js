faf6fd80168f4e3a0db1e6d0de1ed168b7354e99
Justin Tungul <justin.tungul@gmail.com>
2018-07-11 13:17:44 -0700

    Include dependencies in index.html

--- app/app.js
+++ app/app.js
@@ -2,13 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myContacts', [
-  'ngRoute',
-  'myContacts.view1',
-  'myContacts.view2',
-  'myContacts.version'
+  'ngRoute'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/contacts'});
 }]);
